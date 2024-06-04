bc39b0cbfb4a72bb0c3d5a15b0bb21d060a73673
bungamungil <bungamungil@icloud.com>
2024-06-04 21:23:53 +0700

    Improved codes

--- main.cpp
+++ main.cpp
@@ -2,19 +2,73 @@
 #include <iostream>
 using namespace std;
 
+void printHeader();
+string readUserInput(const string& prompt);
+int askUserToChooseMotorcycle();
+void calculatePaymentTerms(
+    double initialPrice,
+    double* interest,
+    double* finalPrice,
+    double* debt,
+    double* paymentPerMonth
+);
+int calculatePercentageInterest(int tenor);
+int askUserToChooseTenor();
+double askUserToInputDownPayment();
+void printPaymentSteps(const string& name, double debt, double paymentPerMonth);
+
 int main() {
+    printHeader();
+    string name = readUserInput("Nama   : ");
+    string address = readUserInput("Alamat : ");
+    int selectedMotorcycle = askUserToChooseMotorcycle();
+    double interest; // Besaran bunga yang akan dibebankan kepada kreditur (dalam Rupiah)
+    double price; // Harga kotor motor setelah ditambahkan bunga (dalam Rupiah)
+    double debt; // Sisa pembayaran yang akan dicicil oleh kreditur (dalam Rupiah)
+    double paymentPerMonth; // Nominal yang akan dibayarkan per bulannya oleh kreditur (dalam Rupiah)
+    cout << fixed << setprecision(2);
+    switch (selectedMotorcycle) {
+        case 1:
+            calculatePaymentTerms(17700000.0, &interest, &price, &debt, &paymentPerMonth);
+            break;
+        case 2:
+            calculatePaymentTerms(20600000.0, &interest, &price, &debt, &paymentPerMonth);
+            break;
+        case 3:
+            calculatePaymentTerms(24800000.0, &interest, &price, &debt, &paymentPerMonth);
+            break;
+        case 4:
+            calculatePaymentTerms(24600000.0, &interest, &price, &debt, &paymentPerMonth);
+            break;
+        case 5:
+            calculatePaymentTerms(18750000.0, &interest, &price, &debt, &paymentPerMonth);
+            break;
+        default:
+            cout << "Terima kasih." << endl;
+            return 0;
+    }
+    printPaymentSteps(name, debt, paymentPerMonth);
+    return 0;
+}
+
+
+void printHeader() {
     cout << "=================================================" << endl;
     cout << "   SELAMAT DATANG DI HONDA SINAR MENTARI MOTOR   " << endl;
     cout << "            Jln. Raya Kaliwungu-Boja             " << endl;
     cout << "=================================================" << endl;
     cout << "Kredit Motor Paling Murah di Sini (^_^)!!        " << endl; // Fixed: Penulisan "di Sini sesuai kaidah Bahasa Indonesia yang baik dan benar
     cout << "=================================================" << endl;
-    cout << "Nama   : ";
-    string name;
-    getline(cin, name);
-    cout << "Alamat : ";
-    string address;
-    getline(cin, address);
+}
+
+string readUserInput(const string& prompt) {
+    cout << prompt;
+    string input;
+    getline(cin, input);
+    return input;
+}
+
+int askUserToChooseMotorcycle() {
     cout << "Daftar Harga Motor :" << endl;
     cout << "1. Beat CBS         Rp 17.700.000" << endl;
     cout << "2. Scoopy           Rp 20.600.000" << endl;
@@ -25,210 +79,63 @@ int main() {
     cout << "Tipe motor yang Anda pilih : ";
     int selectedMotorcycle;
     cin >> selectedMotorcycle;
-    double downPayment; // DP yang diisi oleh calon kreditur (dalam Rupiah)
-    int tenor; // Tenor (lama kredit) yang dipilih calon kreditur (dalam bulan)
-    double initialPrice; // Harga bersih motor yang dipilih calon kreditur (dalam Rupiah)
-    double interest; // Besaran bunga yang akan dibebankan kepada kreditur (dalam Rupiah)
-    double price; // Harga kotor motor setelah ditambahkan bunga (dalam Rupiah)
-    double debt; // Sisa pembayaran yang akan dicicil oleh kreditur (dalam Rupiah)
-    double paymentPerMonth; // Nominal yang akan dibayarkan per bulannya oleh kreditur (dalam Rupiah)
-    cout << fixed << setprecision(2);
-    switch (selectedMotorcycle) {
-        case 1:
-            cout << "======================================================" << endl;
-            cout << "Masukkan DP yang Anda inginkan (minimal Rp 500.000) : ";
-            cin >> downPayment;
-            cout << "======================================================" << endl;
-            if (downPayment >= 500000) {
-                cout << "Pilihan lama kredit yaitu : 12 bulan (bunga 10%)" << endl;
-                cout << "                            24 bulan (bunga 20%)" << endl;
-                cout << "                            36 bulan (bunga 30%)" << endl;
-                cout << "Masukkan lama kredit yang Anda pilih :";
-                cin >> tenor;
-                initialPrice = 17700000.0;
-                if (tenor == 12) {
-                    interest = initialPrice * 10 / 100;
-                    price = initialPrice + interest;
-                    debt = price - downPayment;
-                    paymentPerMonth = debt / tenor;
-                    cout << "Harga motor menjadi " << price << endl;
-                    cout << "Dengan DP " << downPayment << " maka cicilan perbulan = " << (paymentPerMonth) << endl;
-                } else if (tenor == 24) {
-                    interest = initialPrice * 20 / 100;
-                    price = initialPrice + interest;
-                    debt = price - downPayment;
-                    paymentPerMonth = debt / tenor;
-                    cout << "Harga motor menjadi " << price << endl;
-                    cout << "Dengan DP " << downPayment << " maka cicilan perbulan = " << (paymentPerMonth) << endl;
-                } else if (tenor == 36) {
-                    interest = initialPrice * 30 / 100;
-                    price = initialPrice + interest;
-                    debt = price - downPayment;
-                    paymentPerMonth = debt / tenor;
-                    cout << "Harga motor menjadi " << price << endl;
-                    cout << "Dengan DP " << downPayment << " maka cicilan perbulan = " << (paymentPerMonth) << endl;
-                }
-            } else {
-                cout << "DP yang Anda masukkan tidak cukup. Terima kasih." << endl;
-            }
-            break;
-        case 2:
-            cout << "======================================================" << endl;
-            cout << "Masukkan DP yang Anda inginkan (minimal Rp 500.000) : ";
-            cin >> downPayment;
-            cout << "======================================================" << endl;
-            if (downPayment >= 500000) {
-                cout << "Pilihan lama kredit yaitu : 12 bulan (bunga 10%)" << endl;
-                cout << "                            24 bulan (bunga 20%)" << endl;
-                cout << "                            36 bulan (bunga 30%)" << endl;
-                cout << "Masukkan lama kredit yang Anda pilih : ";
-                cin >> tenor;
-                initialPrice = 20600000;
-                if (tenor == 12) {
-                    interest = initialPrice * 10 / 100;
-                    price = initialPrice + interest;
-                    debt = price - downPayment;
-                    paymentPerMonth = debt / tenor;
-                    cout << "Harga motor menjadi " << price << endl;
-                    cout << "Dengan DP " << downPayment << " maka cicilan perbulan = " << (paymentPerMonth) << endl;
-                } else if (tenor == 24) {
-                    interest = initialPrice * 20 / 100;
-                    price = initialPrice + interest;
-                    debt = price - downPayment;
-                    paymentPerMonth = debt / tenor;
-                    cout << "Harga motor menjadi " << price << endl;
-                    cout << "Dengan DP " << downPayment << " maka cicilan perbulan = " << (paymentPerMonth) << endl;
-                } else if (tenor == 36) {
-                    interest = initialPrice * 30 / 100;
-                    price = initialPrice + interest;
-                    debt = price - downPayment;
-                    paymentPerMonth = debt / tenor;
-                    cout << "Harga motor menjadi " << price << endl;
-                    cout << "Dengan DP " << downPayment << " maka cicilan perbulan = " << (paymentPerMonth) << endl;
-                }
-            } else {
-                cout << "DP yang Anda masukkan tidak cukup. Terima kasih." << endl;
-            }
-            break;
-        case 3:
-            cout << "======================================================" << endl;
-            cout << "Masukkan DP yang Anda inginkan (minimal Rp 500.000) : ";
-            cin >> downPayment;
-            cout << "======================================================" << endl;
-            if (downPayment >= 500000) {
-                cout << "Pilihan lama kredit yaitu : 12 bulan (bunga 10%)" << endl;
-                cout << "                            24 bulan (bunga 20%)" << endl;
-                cout << "                            36 bulan (bunga 30%)" << endl;
-                cout << "Masukkan lama kredit yang Anda pilih : ";
-                cin >> tenor;
-                initialPrice = 24800000;
-                if (tenor == 12) {
-                    interest = initialPrice * 10 / 100;
-                    price = initialPrice + interest;
-                    debt = price - downPayment;
-                    paymentPerMonth = debt / tenor;
-                    cout << "Harga motor menjadi " << price << endl;
-                    cout << "Dengan DP " << downPayment << " maka cicilan perbulan = " << (paymentPerMonth) << endl;
-                } else if (tenor == 24) {
-                    interest = initialPrice * 20 / 100;
-                    price = initialPrice + interest;
-                    debt = price - downPayment;
-                    paymentPerMonth = debt / tenor;
-                    cout << "Harga motor menjadi " << price << endl;
-                    cout << "Dengan DP " << downPayment << " maka cicilan perbulan = " << (paymentPerMonth) << endl;
-                } else if (tenor == 36) {
-                    interest = initialPrice * 30 / 100;
-                    price = initialPrice + interest;
-                    debt = price - downPayment;
-                    paymentPerMonth = debt / tenor;
-                    cout << "Harga motor menjadi " << price << endl;
-                    cout << "Dengan DP " << downPayment << " maka cicilan perbulan = " << (paymentPerMonth) << endl;
-                }
-            } else {
-                cout << "DP yang Anda masukkan tidak cukup. Terima kasih." << endl;
-            }
-            break;
-        case 4:
-            cout << "======================================================" << endl;
-            cout << "Masukkan DP yang Anda inginkan (minimal Rp 500.000) : ";
-            cin >> downPayment;
-            cout << "======================================================" << endl;
-            if (downPayment >= 500000) {
-                cout << "Pilihan lama kredit yaitu : 12 bulan (bunga 10%)" << endl;
-                cout << "                            24 bulan (bunga 20%)" << endl;
-                cout << "                            36 bulan (bunga 30%)" << endl;
-                cout << "Masukkan lama kredit yang Anda pilih : ";
-                cin >> tenor;
-                initialPrice = 24600000.0;
-                if (tenor == 12) {
-                    interest = initialPrice * 10 / 100;
-                    price = initialPrice + interest;
-                    debt = price - downPayment;
-                    paymentPerMonth = debt / tenor;
-                    cout << "Harga motor menjadi " << price << endl;
-                    cout << "Dengan DP " << downPayment << " maka cicilan perbulan = " << (paymentPerMonth) << endl;
-                } else if (tenor == 24) {
-                    interest = initialPrice * 20 / 100;
-                    price = initialPrice + interest;
-                    debt = price - downPayment;
-                    paymentPerMonth = debt / tenor;
-                    cout << "Harga motor menjadi " << price << endl;
-                    cout << "Dengan DP " << downPayment << " maka cicilan perbulan = " << (paymentPerMonth) << endl;
-                } else if (tenor == 36) {
-                    interest = initialPrice * 30 / 100;
-                    price = initialPrice + interest;
-                    debt = price - downPayment;
-                    paymentPerMonth = debt / tenor;
-                    cout << "Harga motor menjadi " << price << endl;
-                    cout << "Dengan DP " << downPayment << " maka cicilan perbulan = " << (paymentPerMonth) << endl;
-                }
-            } else {
-                cout << "DP yang Anda masukkan tidak cukup. Terima kasih." << endl;
-            }
-            break;
-        case 5:
-            cout << "======================================================" << endl;
-            cout << "Masukkan DP yang Anda inginkan (minimal Rp 500.000) : ";
-            cin >> downPayment;
-            cout << "======================================================" << endl;
-            if (downPayment >= 500000) {
-                cout << "Pilihan lama kredit yaitu : 12 bulan (bunga 10%)" << endl;
-                cout << "                            24 bulan (bunga 20%)" << endl;
-                cout << "                            36 bulan (bunga 30%)" << endl;
-                cout << "Masukkan lama kredit yang Anda pilih : ";
-                cin >> tenor;
-                initialPrice = 18750000;
-                if (tenor == 12) {
-                    interest = initialPrice * 10 / 100;
-                    price = initialPrice + interest;
-                    debt = price - downPayment;
-                    paymentPerMonth = debt / tenor;
-                    cout << "Harga motor menjadi " << price << endl;
-                    cout << "Dengan DP " << downPayment << " maka cicilan perbulan = " << (paymentPerMonth) << endl;
-                } else if (tenor == 24) {
-                    interest = initialPrice * 20 / 100;
-                    price = initialPrice + interest;
-                    debt = price - downPayment;
-                    paymentPerMonth = debt / tenor;
-                    cout << "Harga motor menjadi " << price << endl;
-                    cout << "Dengan DP " << downPayment << " maka cicilan perbulan = " << (paymentPerMonth) << endl;
-                } else if (tenor == 36) {
-                    interest = initialPrice * 30 / 100;
-                    price = initialPrice + interest;
-                    debt = price - downPayment;
-                    paymentPerMonth = debt / tenor;
-                    cout << "Harga motor menjadi " << price << endl;
-                    cout << "Dengan DP " << downPayment << " maka cicilan perbulan = " << (paymentPerMonth) << endl;
-                }
-            } else {
-                cout << "DP yang Anda masukkan tidak cukup. Terima kasih." << endl;
-                return 0;
-            }
-            break;
-        default:
-            cout << "Terima kasih." << endl;
-            return 0;
+    return selectedMotorcycle;
+}
+
+void calculatePaymentTerms(
+    double initialPrice,
+    double* interest,
+    double* finalPrice,
+    double* debt,
+    double* paymentPerMonth
+) {
+    int tenor = askUserToChooseTenor();
+    double downPayment = askUserToInputDownPayment();
+    int percentageInterest = calculatePercentageInterest(tenor);
+    *interest = initialPrice * percentageInterest / 100;
+    *finalPrice = initialPrice + *interest;
+    *debt = *finalPrice - downPayment;
+    *paymentPerMonth = *debt / tenor;
+    cout << "Harga motor menjadi " << *finalPrice << endl;
+    cout << "Dengan DP " << downPayment << " maka cicilan perbulan = " << *paymentPerMonth << endl;
+}
+
+int calculatePercentageInterest(int tenor) {
+    if (tenor == 12) {
+        return 10;
     }
+    if (tenor == 24) {
+        return 20;
+    }
+    if (tenor == 36) {
+        return 30;
+    }
+    throw invalid_argument("Tenor " + to_string(tenor) + " tidak tersedia.");
+}
+
+int askUserToChooseTenor() {
+    cout << "Pilihan lama kredit yaitu : 12 bulan (bunga 10%)" << endl;
+    cout << "                            24 bulan (bunga 20%)" << endl;
+    cout << "                            36 bulan (bunga 30%)" << endl;
+    cout << "Masukkan lama kredit yang Anda pilih : ";
+    int tenor;
+    cin >> tenor;
+    return tenor;
+}
+
+double askUserToInputDownPayment() {
+    float downPayment;
+    cout << "======================================================" << endl;
+    cout << "Masukkan DP yang Anda inginkan (minimal Rp 500.000) : ";
+    cin >> downPayment;
+    cout << "======================================================" << endl;
+    if (downPayment < 500000) {
+        throw invalid_argument("Down payment " + to_string(downPayment) + " tidak tersedia.");
+    }
+    return downPayment;
+}
+
+void printPaymentSteps(const string& name, double debt, double paymentPerMonth) {
     cout << "=================================================" << endl;
     cout << "Saudara " << name << ", berikut kami tampilkan rincian cicilan Anda :" << endl;
     double debtRemaining = debt;
@@ -239,5 +146,4 @@ int main() {
         cout << "Setelah cicilan ke-" << month << ", sisa pembayaran : " << debtRemaining << endl;
     }
     cout << "Info lebih lanjut hubungi Rina (0859xxxxxxxx)" << endl;
-    return 0;
 }
